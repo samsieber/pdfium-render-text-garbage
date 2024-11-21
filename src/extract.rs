b147088e63bbf3e4fafa766af2032b8bb47dc502
Sam Sieber <swsieber@gmail.com>
2024-11-20 20:28:33 -0600

    Print out data, on a hunch

--- src/extract.rs
+++ src/extract.rs
@@ -1,43 +1,73 @@
-use image::DynamicImage;
+use image::{DynamicImage, GenericImageView};
 use pdfium_render::prelude::{PdfPage, PdfPageObjectCommon, PdfiumError};
 
 pub fn extract_page_image(page: &PdfPage) -> Result<Option<DynamicImage>, PdfiumError> {
-    log::info!("HERE");
     if page.text().unwrap().is_empty() && page.annotations().is_empty() {
-        log::info!("No text no annotations");
         let images = page.objects().create_group(|_o|  true)?;
-        log::info!("Images len: {}", images.len());
         if images.len() == 1 {
             let obj = images.iter().next();
-            log::info!("Iter status: {}", obj.is_some());
             if let Some(image) =  &obj {
                 if let Some(image) = image.as_image_object() {
-                    let bounds = image.bounds()?;
+                    log::debug!("Before bounds");
+                    let bounds = image.bounds()?; 
+                    log::debug!("Before bounds");
                     let media_box = page.boundaries().media()?;
-                    log::info!("{:?}, {:?}", media_box.bounds, bounds);
-                    // if media_box.bounds == bounds {
-                        let image = image.get_raw_image()?;
+                    log::debug!("Going for bitmap");
+                    let image = image.get_raw_image()?;
+                    let data : Vec<u8> = image.pixels().map(|p| p.2.0[0]).collect();
 
-                        log::info!("Found an image of type: {}", match image {
-                            DynamicImage::ImageLuma8(_) => "ImageLuma8",
-                            DynamicImage::ImageLumaA8(_) => "ImageLumaA8",
-                            DynamicImage::ImageRgb8(_) => "ImageRgb8",
-                            DynamicImage::ImageRgba8(_) => "ImageRgba8",
-                            DynamicImage::ImageLuma16(_) => "ImageLuma16",
-                            DynamicImage::ImageLumaA16(_) => "ImageLumaA16",
-                            DynamicImage::ImageRgb16(_) => "ImageRgb16",
-                            DynamicImage::ImageRgba16(_) => "ImageRgba16",
-                            DynamicImage::ImageRgb32F(_) => "ImageRgb32F",
-                            DynamicImage::ImageRgba32F(_) => "ImageRgba32F",
-                            _ => todo!(),
-                        });
-                        return Ok(Some(image))
-                    // }
-                } else {
-                    log::info!("Object type: {:?}",  image.object_type());
+                    process_data(&data[0..2000]);
+
+                    return Ok(Some(image))
                 }
             } 
         }
     }
     Ok(None)
+}
+
+#[derive(PartialEq, Eq)]
+enum LastPush {
+    ASCII,
+    U8,
+    NUL,
+}
+
+/// Prints out bytes as contiguous runs of valid ascii or integer codes of individual bytes, treating NUL as special case
+fn process_data(data: &[u8]) {
+    let mut current_run: Vec<u8> = Vec::new();
+    let mut last_push = LastPush::NUL;
+
+    for &byte in data {
+        if byte == 0 {
+            if last_push == LastPush::ASCII  {
+                log::info!("{:?}", String::from_utf8_lossy(&current_run));
+            } else if last_push == LastPush::U8  {
+                log::info!("{:?} ", &current_run);
+            }
+            last_push = LastPush::NUL;
+            current_run.clear();
+            log::info!("NUL");
+        } else if byte < 128 {
+            if last_push == LastPush::U8 {
+                log::info!("{:?}", &current_run);
+                current_run.clear();
+            }
+            last_push = LastPush::ASCII;
+            current_run.push(byte);
+        } else {
+            if last_push == LastPush::ASCII  {
+                log::info!("{:?}", String::from_utf8_lossy(&current_run));
+                current_run.clear();
+            } 
+            last_push = LastPush::U8;
+            current_run.push(byte);
+        }
+    }
+
+    if last_push == LastPush::ASCII  {
+        log::info!("{:?} ", String::from_utf8_lossy(&current_run))
+    } else if last_push == LastPush::U8  {
+        log::info!("{:?} ", &current_run);
+    }
 }
